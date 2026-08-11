aafcbef86bf787f96cb12616c5d2e778b99ef448
Deivid Santos Lahasse <deividlahasse@gmail.com>
2026-08-11 13:57:39 -0300

    fix: update UserContext error message and add try catch for localStorage

--- src/hooks/UserContext.jsx
+++ src/hooks/UserContext.jsx
@@ -1,3 +1,43 @@
+// import { createContext, useContext, useEffect, useState } from 'react';
+
+// const UserContext = createContext({});
+
+// export const UserProvider = ({ children }) => {
+//   const [userInfo, setUserInfo] = useState({});
+
+//   const putUserData = (userInfo) => {
+//     setUserInfo(userInfo);
+//     localStorage.setItem('devburger:userData', JSON.stringify(userInfo));
+//   };
+
+//   const logout = () => {
+//     setUserInfo({});
+//     localStorage.removeItem('devburger:userData');
+//   };
+
+//   useEffect(() => {
+//     const userInfoLocalStorage = localStorage.getItem('devburger:userData');
+//     if (userInfoLocalStorage) {
+//       setUserInfo(JSON.parse(userInfoLocalStorage));
+//     }
+//   }, []);
+
+//   return (
+//     <UserContext.Provider value={{ userInfo, putUserData, logout }}>
+//       {children}
+//     </UserContext.Provider>
+//   );
+// };
+
+// export const useUser = () => {
+//   const context = useContext(UserContext);
+
+//   if (!context) {
+//     throw new Error('useUser must be a calid context');
+//   }
+
+//   return context;
+// };
 import { createContext, useContext, useEffect, useState } from 'react';
 
 const UserContext = createContext({});
@@ -18,7 +58,12 @@ export const UserProvider = ({ children }) => {
   useEffect(() => {
     const userInfoLocalStorage = localStorage.getItem('devburger:userData');
     if (userInfoLocalStorage) {
-      setUserInfo(JSON.parse(userInfoLocalStorage));
+      try {
+        setUserInfo(JSON.parse(userInfoLocalStorage));
+      } catch (error) {
+        console.error("Erro ao parsear dados do usuário do localStorage", error);
+        localStorage.removeItem('devburger:userData');
+      }
     }
   }, []);
 
@@ -33,7 +78,7 @@ export const useUser = () => {
   const context = useContext(UserContext);
 
   if (!context) {
-    throw new Error('useUser must be a calid context');
+    throw new Error('useUser must be a valid context');
   }
 
   return context;
